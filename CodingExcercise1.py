dca06612c33e173e4e41d0047b77d068d852f74a
Gyula80 <szedlak.gyula@gmail.com>
2022-11-15 09:50:32 +0000

    First Commit

---
+++ CodingExcercise1.py
@@ -0,0 +1,28 @@
+import PySimpleGUI as ps
+
+label_feet = ps.Text('Enter feet:')
+textbox_feet = ps.InputText('', key = 'feet')
+label_inch = ps.Text('Enter inches')
+textbox_inch = ps.InputText('', key = 'inch')
+button = ps.Button('convert', key = 'convert')
+result_label = ps.Text('',key = 'result')
+
+window = ps.Window('Convertor', layout = [[label_feet, textbox_feet],
+                                          [label_inch, textbox_inch],
+                                          [button, result_label] ])
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case ps.WIN_CLOSED:
+            break
+        case 'convert':
+            in_meter = float(values['feet']) * 0.3048 + float(values['inch']) * 0.0254
+
+            ###window['result'].update(str(in_meter) + ' m')
+            window['result'].update(value=f"{in_meter} m", text_color="pink")
+
+window.close()
+
