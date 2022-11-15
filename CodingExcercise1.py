b6c3f999fd0e790aedbe3266c8eaa50c610ad3e4
Gyula80 <szedlak.gyula@gmail.com>
2022-11-15 09:58:12 +0000

    added features

--- CodingExcercise1.py
+++ CodingExcercise1.py
@@ -13,16 +13,15 @@ window = ps.Window('Convertor', layout = [[label_feet, textbox_feet],
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+
     match event:
         case ps.WIN_CLOSED:
             break
         case 'convert':
             in_meter = float(values['feet']) * 0.3048 + float(values['inch']) * 0.0254
-
-            ###window['result'].update(str(in_meter) + ' m')
-            window['result'].update(value=f"{in_meter} m", text_color="pink")
+            print('hello')
+            window['result'].update(str(in_meter) + ' m')
+            ###window['result'].update(value=f"{in_meter} m", text_color="pink")
 
 window.close()
 
